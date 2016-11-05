bd29dd4cef1504518f75dcc905a774e6a5b1dc93
ccn.berkeley <ccn.berkeley@gmail.com>
2016-11-05 14:28:36 -0700

    switch even when there are nans

--- ProbabilisticSwitching/maybe_switch_sides.m
+++ ProbabilisticSwitching/maybe_switch_sides.m
@@ -2,15 +2,15 @@ function maybe_switch_sides(trial)
 
 global exp
 
-% Determine when the last switch happened (if none yet, set to trial 1)
-last_switch_trial = max(find(exp.PROBSWITCHdata.switch_trial == 1));
+% Determine when the last switch happened (if none yet, set to 1)
+last_switch_trial = find(exp.PROBSWITCHdata.switch_trial == 1, 1);
 if isempty(last_switch_trial)
     last_switch_trial = 1;
 end
 
 % Check if participant has gained the predetermind number of rewards
 % for this run
-number_rewards_in_run = sum(exp.PROBSWITCHdata.reward(last_switch_trial:trial-1));
+number_rewards_in_run = nansum(exp.PROBSWITCHdata.reward(last_switch_trial:trial-1));
 if number_rewards_in_run == exp.run_length(exp.run)
     exp.better_box_left = 1 - exp.better_box_left;  % Switch side of the better box
     exp.switch_trial = 1;   % Remember that this was a switch trial (reset to 0 in init_trial)
